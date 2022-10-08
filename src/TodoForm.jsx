8cf7382f7ac91db4663890a8b67893f7309465c5
Sunil Joshi <joshisunil2601@gmail.com>
2022-10-08 14:06:10 +0530

    cancel bug

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -11,6 +11,8 @@ export const TodoForm = (props) => {
 
   const handleSave = () => {
     if (input.trim().length == 0) {
+      alert("please enter a todo massage");
+
       return;
     }
     props.onClose();
@@ -21,10 +23,12 @@ export const TodoForm = (props) => {
   return (
     <div className="p-4 border  border-gray-300 shadow-md my-4 rounded-md bg-green-200">
       <h3 className="py-4">Create a todo</h3>
-      <form onSubmit={handleSave}>
+      <form onSubmit={props.onClose}>
         <Input value={input} onChange={handleInputChange} />
         <div className="flex gap-6">
-          <Button disabled={!input}>save</Button>
+          <Button disabled={!input} onClick={handleSave}>
+            save
+          </Button>
           <Button them="secondary">Cancel</Button>
         </div>
       </form>
