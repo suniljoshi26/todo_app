5cab289669478113c92c7d1d51e8cf33689fbfc4
Sunil Joshi <joshisunil2601@gmail.com>
2022-10-08 10:54:25 +0530

    empty bug fix if

--- src/TodoForm.jsx
+++ src/TodoForm.jsx
@@ -22,12 +22,10 @@ export const TodoForm = (props) => {
   return (
     <div className="p-4 border  border-gray-300 shadow-md my-4 rounded-md bg-green-200">
       <h3 className="py-4">Create a todo</h3>
-      <form onSubmit={props.onClose}>
+      <form onSubmit={handleSave}>
         <Input value={input} onChange={handleInputChange} />
         <div className="flex gap-6">
-          <Button disabled={input.trim().length == 0} onClick={handleSave}>
-            save
-          </Button>
+          <Button disabled={!input}>save</Button>
           <Button them="secondary">Cancel</Button>
         </div>
       </form>
